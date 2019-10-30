50c1c9e5242667b5b0e778a6bcbf14b2a6bb3cd8
Dan Murphy <danielmurph8@gmail.com>
2019-10-30 14:01:31 -0400

    Add files via upload

---
+++ restartBoard_Oct22.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.print("Restarting in 10 seconds");
+
+  delay(10000);
+  ESP.restart();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+} 
